6b4397197a3f394dd1bd5dfdd87fb1392c92025a
Khojayev12 <khojayev12@gmail.com>
2025-11-21 11:55:22 +0500

    profile page desktop version ready

--- dbhandler.py
+++ dbhandler.py
@@ -5,7 +5,7 @@ class DBHandler():
     def __init__(self):
         try:
             self.cnx = mysql.connector.connect(user='root', password='Scripter1211#',
-                                host='localhost',
+                                host='khoja.jprq.live', port='37445',
                                 database='recipedb')
             self.cursor = self.cnx.cursor()
             print("DBhandler initiated")
